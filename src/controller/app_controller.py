146dcff86c98180158106f6622bad4c189845ef6
Fruitsi <fruitsiii01@gmail.com>
2024-08-02 16:19:41 +0200

    Updated controller to add new window

--- src/controller/app_controller.py
+++ src/controller/app_controller.py
@@ -3,16 +3,26 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from view.start_menu import StartMenu
+from view.exchange_menu import ExchangeMenu  # Import the new menu screen
 
 class StartMenuScreen(Screen):
     def __init__(self, **kwargs):
         super(StartMenuScreen, self).__init__(**kwargs)
         self.add_widget(StartMenu())
 
+class ExchangeWindowScreen(Screen):
+    def __init__(self, **kwargs):
+        super(ExchangeWindowScreen,self).__init__(**kwargs)
+        self.add_widget(ExchangeMenu())
+
 class AppController(App):
     def build(self):
         self.screen_manager = ScreenManager()
+        
+        # Add existing and new screens
         self.screen_manager.add_widget(StartMenuScreen(name='start_menu'))
+        self.screen_manager.add_widget(ExchangeWindowScreen(name='exchange_window'))
+        
         return self.screen_manager
 
 if __name__ == '__main__':
