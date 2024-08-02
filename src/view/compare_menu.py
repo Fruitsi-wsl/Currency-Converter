cbbcd9667af054496cfaefa928357106b3946ffa
Fruitsi <fruitsiii01@gmail.com>
2024-08-02 21:19:18 +0200

    Added feature to allow selection of 2 currencies inside compare menu

--- src/view/compare_menu.py
+++ src/view/compare_menu.py
@@ -7,12 +7,10 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.image import Image
 from kivy.uix.label import Label
 from kivy.uix.button import Button
-from kivy.uix.scrollview import ScrollView
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.textinput import TextInput
-from kivy.graphics import Color, RoundedRectangle
+from kivy.uix.spinner import Spinner
 from model.get_exchange_rate_data import fetch_exchange_rates
-from kivy.uix.widget import Widget
 from controller import shared_variables as shared_variables
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -45,6 +43,84 @@ class CompareMenu(FloatLayout):
         )
         self.back_button.bind(on_press=self.on_back_button_press)
         self.add_widget(self.back_button)
+
+
+
+        # First currency selection
+        self.currency1_input = TextInput(
+            hint_text="Search currency",
+            size_hint=(None, None), size=(150,35),
+            pos_hint={'center_x': 0.35, 'center_y': 0.8},
+            multiline=False
+        )
+        self.currency1_input.bind(text=self.update_currency1_options)
+        self.add_widget(self.currency1_input)
+
+        self.currency1_spinner = Spinner(
+            text="Select Currency",
+            values=[],  # This will be updated with available currencies
+            size_hint=(None, None),size=(150,50),
+            background_color=(24/255, 198/255, 128/255, 1),
+            color=(166/255, 227/255, 237/255, 1),
+            pos_hint={'center_x': 0.35, 'center_y': 0.7}
+            
+        )
+        self.add_widget(self.currency1_spinner)
+
+        # Second currency selection
+        self.currency2_input = TextInput(
+            hint_text="Search currency",
+            size_hint=(None, None),size=(150,35),
+            pos_hint={'center_x': 0.65, 'center_y': 0.8},
+            multiline=False
+        )
+        self.currency2_input.bind(text=self.update_currency2_options)
+        self.add_widget(self.currency2_input)
+
+        self.currency2_spinner = Spinner(
+            text="Select Currency",
+            background_color=(24/255, 198/255, 128/255, 1),
+            color=(166/255, 227/255, 237/255, 1),
+            values=[],  # This will be updated with available currencies
+            pos_hint={'center_x': 0.65, 'center_y': 0.7},
+            size_hint=(None, None),size=(150,50),
+        )
+        self.add_widget(self.currency2_spinner)
+
+
+        self.back_button = Button(
+            text="Back",
+            background_color=(24/255, 198/255, 128/255, 1),
+            color=(166/255, 227/255, 237/255, 1),
+            size_hint=(None, None), size=(150, 50),
+            pos_hint={'center_x': 0.1, 'center_y': 0.9}
+        )
+        self.back_button.bind(on_press=self.on_back_button_press)
+        self.add_widget(self.back_button)
+
+        # Fetch initial exchange rates to populate spinners
+        self.all_rates = self.fetch_initial_exchange_rates()
+        self.update_currency1_options()
+        self.update_currency2_options()
+
+
+
+    def fetch_initial_exchange_rates(self):
+        try:
+            data = fetch_exchange_rates(API_KEY)
+            return data.get('conversion_rates', {}).keys()
+        except requests.RequestException as e:
+            return []
+
+    def update_currency1_options(self, instance=None, value=''):
+        search_query = value.upper()
+        filtered_currencies = [currency for currency in self.all_rates if search_query in currency]
+        self.currency1_spinner.values = filtered_currencies
+
+    def update_currency2_options(self, instance=None, value=''):
+        search_query = value.upper()
+        filtered_currencies = [currency for currency in self.all_rates if search_query in currency]
+        self.currency2_spinner.values = filtered_currencies
 
     def on_back_button_press(self, instance):
         app = App.get_running_app()
