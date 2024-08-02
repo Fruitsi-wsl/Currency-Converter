8ae21f81a5443cfa0082bc7efb61448217e2a63a
Fruitsi <fruitsiii01@gmail.com>
2024-08-02 16:19:18 +0200

    Added echange_menu.py this menu will be used to Show different echange rates of currencies

--- src/view/exchange_menu.py
+++ src/view/exchange_menu.py
@@ -9,35 +9,33 @@ from kivy.uix.image import Image
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
-image_path = os.path.join(current_dir, "Image Resources/background2.jpg" )
+image_path = os.path.join(current_dir, "Image Resources/background1.jpg" )
 
-class StartMenu(FloatLayout):
+class ExchangeMenu(FloatLayout):
     def __init__(self, **kwargs):
-        super(StartMenu, self).__init__(**kwargs)
+        super(ExchangeMenu, self).__init__(**kwargs)
 
         self.background = Image(source=image_path, allow_stretch=True, keep_ratio=False)
         self.add_widget(self.background)
-        
-        # Label at the top center
+
+        # Example content for the new menu
         self.add_widget(Label(
             text="Currency Converter", font_size='24sp',
             color=(24/255, 198/255, 128/255, 1),
             size_hint=(None, None), size=(200, 50),
             pos_hint={'center_x': 0.5, 'top': 1}
         ))
-        
-        # Button at the center
-        self.start_button = Button(
-            text="Start",
+
+        self.back_button = Button(
+            text="Back",
             background_color=(24/255, 198/255, 128/255, 1),
             color=(166/255, 227/255, 237/255, 1),
-            size_hint=(None, None), size=(200, 50),
-            pos_hint={'center_x': 0.5, 'center_y': 0.2}
+            size_hint=(None, None), size=(150, 50),
+            pos_hint={'center_x': 0.1, 'center_y': 0.9}
         )
-        self.start_button.bind(on_press=self.on_start_button_press)
-        self.add_widget(self.start_button)
+        self.back_button.bind(on_press=self.on_back_button_press)
+        self.add_widget(self.back_button)
         
-    def on_start_button_press(self, instance):
+    def on_back_button_press(self, instance):
         app = App.get_running_app()
-        app.screen_manager.current = 'exchange_window'
-
+        app.screen_manager.current = 'start_menu'
