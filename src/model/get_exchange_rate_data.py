84e4516822a8d98fb0b3ffd8ae21c2bef6df9ac6
Fruitsi <fruitsiii01@gmail.com>
2024-08-02 18:07:48 +0200

    Added function to get exchange rate between 2 specific currencies

--- src/model/get_exchange_rate_data.py
+++ src/model/get_exchange_rate_data.py
@@ -7,3 +7,9 @@ def fetch_exchange_rates(api_key):
     response = requests.get(url)
     response.raise_for_status()  # Raise an exception for HTTP errors
     return response.json() 
+
+def get_specific_rates(api_key,first_currency,second_currency):
+    url = f'https://v6.exchangerate-api.com/v6/{api_key}/pair/{first_currency}/{second_currency}'
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.json()
